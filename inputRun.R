58e0740239b030b3161154f1de5cf970fc858a74
SolonJ-bio <solon.jiang@gmail.com>
2025-03-09 16:06:29 +0800

    'update'

--- inputRun.R
+++ inputRun.R
@@ -1,100 +1,129 @@
-library(dplyr)
-library(Seurat)
+suppressMessages(library(dplyr))
+suppressMessages(library(Seurat))
+suppressMessages(library(immunarch))
+suppressMessages(library(caret))
+suppressMessages(library(glmnet))
+suppressMessages(library(xgboost))
+suppressMessages(library(randomForest))
+suppressMessages(library(nnet))
+suppressMessages(library(pROC))
 
-source('models.R')
+# =========================================================
+# Load methods for traning model
 
-#=========================================================
+source("models.R")
+
+# =========================================================
 # Load K-mers data
 
-kmers.df <- readRDS('kmers.df.RDS')
+getKmers <- function(train.input, test.input, kmers = 5) {
+    if (dir.exists(train.input)) {
+        train.files <- list.files(train.input, full.names = TRUE)
+        train.input <- lapply(train.files, function(x.fil) {
+            read.table(x.fil, header = TRUE) %>% `colnames<-`(c("CDR3.aa", "Abundance"))
+        }) %>%
+            `names<-`(gsub("\\.tsv", "", basename(train.files)))
+    }
+    if (dir.exists(test.input)) {
+        test.files <- list.files(test.input, full.names = TRUE)
+        test.input <- lapply(test.files, function(x.fil) {
+            read.table(x.fil, header = TRUE) %>% `colnames<-`(c("CDR3.aa", "Abundance"))
+        }) %>%
+            `names<-`(gsub("\\.tsv", "", basename(test.files)))
+    }
+    if (!inherits(train.input, list)) {
+        stop("Input type error: Please provide a directory path containing CDR3 sequences or a list of CDR3 sequences for each sample.")
+    }
+    raw.test.lab <- names(test.input)
+    names(test.input) <- paste0(names(test.input), "_X_", 1:length(test.input))
+    entire.motif <- list(
+        data = c(train.input, test.input),
+        meta = data.frame(
+            CANCER.TYPE = c(gsub("_.*", "", names(train.input)), rep("TEST", length(test.input)))
+        ) %>% `rownames<-`(c(names(train.input), names(test.input)))
+    )
+    kmers.df <- getKmers(entire.motif$data, kmers) %>% as.data.frame()
+    kmers.df[is.na(kmers.df)] <- 0
+    rownames(kmers.df) <- kmers.df[, 1]
+    kmers.df <- kmers.df[, -1]
+    train.motif <- list(data = kmers.df[, entire.motif$meta$CANCER.TYPE != "TEST"], meta = subset(entire.motif$meta, CANCER.TYPE != "TEST"))
+    test.motif <- kmers.df[, entire.motif$meta$CANCER.TYPE == "TEST"] %>% `colnames<-`(raw.test.lab)
+    return(list(train = train.motif, test = test.motif))
+}
 
-obj <- CreateSeuratObject(counts = kmers.df, meta.data = meta.data %>% data.frame) %>% NormalizeData
-Idents(obj) <- obj$cancer_type
-de.motifs <- FindAllMarkers(obj, only.pos = TRUE, min.pct = 0.05, logfc.threshold = 0.01, return.thresh = 0.01)
-kmers.df.sub <- kmers.df.sub.norm <- GetAssayData(obj) %>% as.data.frame %>% .[rownames(de.motifs), ] %>% t
+degKmers <- function(train.motif, logfc.thresh = 0, min.pct = 0.01, pval.thresh = 0.01) {
+    obj <- CreateSeuratObject(counts = kmers.df, meta.data = train.motif$meta) %>% NormalizeData()
+    Idents(obj) <- obj$CANCER.TYPE
+    de.motifs <- FindAllMarkers(
+        obj,
+        only.pos = TRUE,
+        logfc.threshold = logfc.thresh,
+        return.thresh = pval.thresh,
+        min.pct = min.pct
+    )
+    kmers.df.sub <- GetAssayData(obj) %>%
+        as.data.frame() %>%
+        .[rownames(de.motifs), ] %>%
+        t()
+    return(kmers.df.sub)
+}
 
-set.seed(2024)
-train.sns <- sample(rownames(kmers.df.sub), floor(nrow(kmers.df.sub) * 0.5))
-test.sns <- setdiff(rownames(kmers.df.sub), train.sns)
-table(sn.labels[test.sns])#
-table(sn.labels[train.sns])
+trainModel <- function(train.motif,
+                       train.bert = NULL,
+                       pos.lab = "Patient",
+                       neg.labe = "Health",
+                       kmers = 5,
+                       ndims = 50,
+                       logfc.thresh = 0,
+                       pval.thresh = 0.01) {
+    kmers.df.sub <- degKmers(train.motif, logfc.thresh = logfc.thresh, pval.thresh = pval.thresh)
+    pca.motif <- prcomp(kmers.df.sub, center = TRUE, scale. = FALSE)
+    train.motif.pca <- pca.motif$x[, 1:ndims] %>% scale()
+    if (!is.null(train.bert)) {
+        pca.bert <- prcomp(train.bert, center = TRUE, scale. = TRUE)
+        train.bert <- pca_result$x[, 1:50]
+    }
+    # ovp.sns <- intersect(rownames(train.motif), rownames(train.bert))
+    if (!is.null(train.bert)) {
+        train.data <- list(
+            Motif = train.motif.pca[ovp.sns, ],
+            Bert = train.bert[ovp.sns, ]
+        )
+    } else {
+        train.data <- list(Motif = train.motif.pca)
+    }
+    train.labels <- ifelse(train.motif$meta$CANCER.TYPE == pos.lab, 1, 0)
+    base.models <- trainFirstLayer(train.data, train.labels)
+    train.base.predictions <- getBasePredictions(base.models, train.data)
+    stack.models <- trainSecondLayer(train.base.predictions, train.labels)
+    return(list(base.model = base.models, stack.model = stacked.models, pca.motif = pca.motif, pca.bert = pca.bert))
+}
 
-selected.kmers <- rownames(de.motifs)
-pca_result <- prcomp(kmers.df.sub, center = F, scale. = T)
-pca_data <- as.data.frame(pca_result$x)
-pca_data$Species <- immdata$meta$cancer_type
+predictRes <- function(test.motif, train.models, test.bert = NULL) {
+    test.motif.scaled <- scale(test.motif, center = train.models$pca.motif$center, scale = train.models$pca.motif$scale)
+    test.motif.pca <- test.motif.scaled %*% train.models$pca.motif$rotation[, 1:100]
+    if (!is.null(test.bert)) {
+        test.bert.scaled <- scale(test.bert, center = pca.bert$center, scale = pca.bert$scale)
+        test.bert <- test.bert.scaled %*% pca.bert$rotation[, 1:100]
+    }
+    if (!is.null(test.bert)) {
+        test.data <- list(
+            Motif = test.motif[ovp.sns, ],
+            Bert = test.bert[ovp.sns, ]
+        )
+    } else {
+        train.data <- list(Motif = test.motif.pca)
+    }
+    test.base.pred <- getBasePredictions(base.models, test_data)
+    predictions <- predict_stack_models(stacked.models, test.base.pred)
+    predictions <- ifelse(predictions > 0.9, 1, 0)
+}
 
-train.dat <- kmers.df.sub[train.sns, ]
-pca_result <- prcomp(train.dat, center = TRUE, scale. = FALSE)
-# Get the reduced feature matrix
-train.motif <- reduced_matrix.motif <- reduced_matrix <- pca_result$x[, 1:50] %>% scale
+evaluatePredct <- function() {
+    train_scores <- predictions <- rowMeans(train_base_predictions)
 
-#==================================================================
-# ProteinBERT
-
-extracted.infos <- lapply(names(immdata$data), function(xx) {
-    tmp.data <- immdata$data[[xx]]
-    tmp.data <- tmp.data %>% filter(nchar(CDR3.aa) >= 10 & nchar(CDR3.aa) <= 24)
-    tmp.df <- tmp.data[, c('CDR3.aa', 'Proportion')] %>% `colnames<-`(c('TCR', 'Abundance'))
-}) %>% `names<-`(names(immdata$data))
-extracted.infos[[1]] %>% head
-
-dir.create('train.01', showWarnings = FALSE)
-dir.create('test.01', showWarnings = FALSE)
-dir.create('train.01.pth', showWarnings = FALSE)
-dir.create('test.01.pth', showWarnings = FALSE)
-
-train.lst <- extracted.infos[train.sns]
-status <- lapply(names(train.lst), function(xx) {
-    label <- sn.labels[xx]
-    tcr <- tcr.labels[xx] 
-    idx <- which(names(sn.labels) == xx)
-    if (nrow(train.lst[[xx]]) >= 200) write.table(train.lst[[xx]][1: 200, ], file = file.path('train.01', paste0(label, '_', tcr, '_', idx, '.tsv')), sep = '\t', quote = FALSE, row.names = FALSE)
-})
-
-test.lst <- extracted.infos[test.sns]
-status <- lapply(names(test.lst), function(xx) {
-    label <- sn.labels[xx]
-    tcr <- tcr.labels[xx]
-    idx <- which(names(sn.labels) == xx)
-    if (nrow(test.lst[[xx]]) >= 200) write.table(test.lst[[xx]][1: 200, ], file = file.path('test.01', paste0(label, '_', tcr, '_', idx, '.tsv')), sep = '\t', quote = FALSE, row.names = FALSE)
-})
-
-files <- list.files('train.01.pth/', full.names = TRUE)
-
-files <- list.files('train.01.pth/', full.names = TRUE)
-row.names <- gsub('.*_|\\.tsv', '', files) %>% as.integer %>% names(sn.labels)[.]
-train.bert <- lapply(files, function(fil) {
-    df <- read.table(fil, sep = '\t') %>% scale
-    as.vector(as.matrix(df))
-}) %>% do.call(cbind, .) %>% as.data.frame %>% t %>% `rownames<-`(row.names)
-
-train_bert.labels <- ifelse(gsub('.*//|_.*', '', files) == 'CRC', 1, 0)
-
-pca_result <- prcomp(train.bert, center = TRUE, scale. = TRUE)
-train.bert <- reduced_matrix <- pca_result$x[, 1:50]
-
-files <- list.files('test.01.pth/', full.names = TRUE)
-row.names <- gsub('.*_|\\.tsv', '', files) %>% as.integer %>% names(sn.labels)[.]
-test.bert <- lapply(files, function(fil) {
-    df <- read.table(fil, sep = '\t') %>% scale
-    as.vector(as.matrix(df))
-}) %>% do.call(cbind, .) %>% as.data.frame %>% t %>% `rownames<-`(row.names)
-test_labels <- ifelse(gsub('.*//|_.*', '', files) == 'CRC', 1, 0)
-
-test_data_scaled <- scale(test.bert, center = pca_result$center, scale = pca_result$scale)
-test.bert <- test_data_scaled %*% pca_result$rotation[, 1:100]
-
-#================================================================
-# Train model
-
-source('model.R')
-ovp.sns <- intersect(rownames(train.motif), rownames(train.bert))
-train_data <- list(
-  Motif = train.motif[ovp.sns, ],
-  Bert = train.bert[ovp.sns, ]
-)
-
-train_base_predictions <- get_base_predictions(base_models, train_data)
-head(train_base_predictions)
-stack_models <- train_second_layer(train_base_predictions, train_bert.labels)
+    predictions <- ifelse(predictions > 0.5, 1, 0)
+    predictions <- factor(predictions, levels = c(0, 1))
+    trainLabels <- factor(train_bert.labels, levels = c(0, 1))
+    confusionMatrix(predictions, trainLabels)
+}
---
+++ models.R
@@ -0,0 +1,272 @@
+# Required packages
+
+#' First Layer Model Training Function
+#' @param feature_data List of feature matrices
+#' @param labels Vector of binary labels
+#' @param k Number of top models to select per feature
+#' @return List of trained base models
+trainFirstLayer <- function(feature_data, labels, k = 5) {
+    base_models <- list()
+
+    for (feature_name in names(feature_data)) {
+        feature_matrix <- feature_data[[feature_name]]
+
+        # Parameter grids for each algorithm
+        param_grids <- list(
+            glm = expand.grid(
+                alpha = seq(0, 1, 0.2)
+            ),
+            xgboost = expand.grid(
+                nrounds = c(100, 200),
+                max_depth = c(3, 6),
+                eta = c(0.01, 0.1),
+                gamma = c(0, 0.1),
+                subsample = c(0.7, 0.8, 0.9)
+            ),
+            rf = expand.grid(
+                mtry = c(2, 4, 6),
+                ntree = c(500, 1000)
+            ),
+            nn = expand.grid(
+                size = c(3, 4, 5),
+                decay = c(0.001, 0.01, 0.1)
+            )
+        )
+
+        # Store results for each model
+        results <- list()
+
+        # GLM
+        for (i in 1:nrow(param_grids$glm)) {
+            set.seed(123)
+            # Let cv.glmnet choose lambda sequence automatically
+            cv_fit <- cv.glmnet(as.matrix(feature_matrix), labels,
+                alpha = param_grids$glm$alpha[i],
+                family = "binomial"
+            )
+            # Use lambda.min for predictions
+            pred <- predict(cv_fit, as.matrix(feature_matrix),
+                s = "lambda.min", type = "response"
+            )
+            auc <- auc(roc(labels, as.vector(pred)))
+            results[[paste0("glm_", i)]] <- list(
+                model = cv_fit, auc = auc,
+                params = param_grids$glm[i, ]
+            )
+        }
+
+        # XGBoost
+        for (i in 1:nrow(param_grids$xgboost)) {
+            set.seed(123)
+            dtrain <- xgb.DMatrix(data = as.matrix(feature_matrix), label = labels)
+            model <- xgboost(
+                data = dtrain,
+                nrounds = param_grids$xgboost$nrounds[i],
+                max_depth = param_grids$xgboost$max_depth[i],
+                eta = param_grids$xgboost$eta[i],
+                gamma = param_grids$xgboost$gamma[i],
+                subsample = param_grids$xgboost$subsample[i],
+                objective = "binary:logistic",
+                verbose = 0
+            )
+            pred <- predict(model, as.matrix(feature_matrix))
+            auc <- auc(roc(labels, pred))
+            results[[paste0("xgb_", i)]] <- list(
+                model = model, auc = auc,
+                params = param_grids$xgboost[i, ]
+            )
+        }
+
+        # Random Forest
+        for (i in 1:nrow(param_grids$rf)) {
+            set.seed(123)
+            model <- randomForest(
+                x = feature_matrix, y = as.factor(labels),
+                mtry = param_grids$rf$mtry[i],
+                ntree = param_grids$rf$ntree[i]
+            )
+            pred <- predict(model, feature_matrix, type = "prob")[, 2]
+            auc <- auc(roc(labels, pred))
+            results[[paste0("rf_", i)]] <- list(
+                model = model, auc = auc,
+                params = param_grids$rf[i, ]
+            )
+        }
+
+        # Neural Network
+        for (i in 1:nrow(param_grids$nn)) {
+            set.seed(123)
+            model <- nnet(feature_matrix, labels,
+                size = param_grids$nn$size[i],
+                decay = param_grids$nn$decay[i],
+                maxit = 200,
+                trace = FALSE
+            )
+            pred <- predict(model, feature_matrix)
+            auc <- auc(roc(labels, pred))
+            results[[paste0("nn_", i)]] <- list(
+                model = model, auc = auc,
+                params = param_grids$nn[i, ]
+            )
+        }
+
+        # Select top k models for this feature type
+        aucs <- sapply(results, function(x) x$auc)
+        top_models <- results[order(aucs, decreasing = TRUE)[1:k]]
+        base_models[[feature_name]] <- top_models
+    }
+
+    return(base_models)
+}
+
+#' Generate predictions from first layer models
+#' @param base_models List of trained base models from first layer
+#' @param feature_data List of feature matrices
+#' @return Matrix of predictions from all base models
+getBasePredictions <- function(base_models, feature_data) {
+    n_samples <- nrow(feature_data[[1]])
+    total_models <- sum(sapply(base_models, length))
+    base_predictions <- matrix(0, nrow = n_samples, ncol = total_models)
+    colnames(base_predictions) <- character(total_models)
+
+    col_idx <- 1
+    for (feature_name in names(base_models)) {
+        feature_matrix <- feature_data[[feature_name]]
+        feature_models <- base_models[[feature_name]]
+
+        for (model_name in names(feature_models)) {
+            model_info <- feature_models[[model_name]]
+            model <- model_info$model
+
+            if (inherits(model, "cv.glmnet")) {
+                pred <- predict(model,
+                    newx = as.matrix(feature_matrix),
+                    s = "lambda.min", type = "response"
+                )
+            } else if (inherits(model, "xgb.Booster")) {
+                pred <- predict(model, as.matrix(feature_matrix))
+            } else if (inherits(model, "randomForest")) {
+                pred <- predict(model, feature_matrix, type = "prob")[, 2]
+            } else if (inherits(model, "nnet")) {
+                pred <- predict(model, feature_matrix)
+            }
+
+            base_predictions[, col_idx] <- as.vector(pred)
+            colnames(base_predictions)[col_idx] <- paste0(feature_name, "_", model_name)
+            col_idx <- col_idx + 1
+        }
+    }
+
+    return(base_predictions)
+}
+
+#' Second Layer Model Training Function
+#' @param base_predictions Matrix of predictions from base models
+#' @param labels Vector of binary labels
+#' @param n_models Number of final models to ensemble
+#' @return List of trained stacking models
+trainSecondLayer <- function(base_predictions, labels, n_models = 10) {
+    # Parameter grids for stacking algorithms
+    param_grids <- list(
+        glm = expand.grid(
+            alpha = seq(0, 1, 0.2)
+        ),
+        xgboost = expand.grid(
+            nrounds = c(100, 200),
+            max_depth = c(3, 6),
+            eta = c(0.01, 0.1)
+        ),
+        rf = expand.grid(
+            mtry = c(2, 4, 6),
+            ntree = c(500, 1000)
+        )
+    )
+
+    # Store results for each stacking model
+    stack_results <- list()
+
+    # Train stacking models
+    for (algorithm in c("glm", "xgboost", "rf")) {
+        params <- param_grids[[algorithm]]
+
+        for (i in 1:nrow(params)) {
+            set.seed(123)
+
+            if (algorithm == "glm") {
+                model <- cv.glmnet(as.matrix(base_predictions), labels,
+                    alpha = params$alpha[i],
+                    family = "binomial"
+                )
+                pred <- predict(model, as.matrix(base_predictions),
+                    s = "lambda.min", type = "response"
+                )
+            } else if (algorithm == "xgboost") {
+                dtrain <- xgb.DMatrix(data = as.matrix(base_predictions), label = labels)
+                model <- xgboost(
+                    data = dtrain,
+                    nrounds = params$nrounds[i],
+                    max_depth = params$max_depth[i],
+                    eta = params$eta[i],
+                    objective = "binary:logistic",
+                    verbose = 0
+                )
+                pred <- predict(model, as.matrix(base_predictions))
+            } else if (algorithm == "nnet") {
+                model <- nnet(feature_matrix, labels,
+                    size = params$size[i],
+                    decay = params$decay[i],
+                    maxit = 200,
+                    trace = FALSE
+                )
+                pred <- predict(model, feature_matrix)
+            } else {
+                model <- randomForest(
+                    x = base_predictions, y = as.factor(labels),
+                    mtry = params$mtry[i],
+                    ntree = params$ntree[i]
+                )
+                pred <- predict(model, base_predictions, type = "prob")[, 2]
+            }
+
+            # Calculate AUC
+            auc <- auc(roc(labels, as.vector(pred)))
+            stack_results[[paste0(algorithm, "_", i)]] <- list(
+                model = model,
+                auc = auc,
+                params = params[i, ]
+            )
+        }
+    }
+
+    # Select top n_models
+    aucs <- sapply(stack_results, function(x) x$auc)
+    top_models <- stack_results[order(aucs, decreasing = TRUE)[1:n_models]]
+    return(top_models)
+}
+
+predictStackModels <- function(stack_models, new_data) {
+    # Store predictions from each model
+    model_predictions <- numeric(nrow(new_data))
+
+    # Iterate through each base model and make predictions
+    for (model_name in names(stack_models)) {
+        model_info <- stack_models[[model_name]]
+        model <- model_info$model
+        if (inherits(model, "cv.glmnet")) {
+            pred <- predict(model,
+                newx = as.matrix(feature_matrix),
+                s = "lambda.min", type = "response"
+            )
+        } else if (inherits(model, "xgb.Booster")) {
+            pred <- predict(model, as.matrix(feature_matrix))
+        } else if (inherits(model, "randomForest")) {
+            pred <- predict(model, feature_matrix, type = "prob")[, 2]
+        } else if (inherits(model, "nnet")) {
+            pred <- predict(model, feature_matrix)
+        }
+        model_predictions <- model_predictions + as.vector(pred)
+    }
+    # Average the predictions across all models
+    final_prediction <- model_predictions / length(stack_models)
+    return(final_prediction)
+}
